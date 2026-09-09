370f7303536988d0fcbfe0ccbef86241d3478dd5
rosan-f <fikrirausyan131@gmail.com>
2026-09-09 20:05:32 +0700

    feat: make tcp client target host dynamic with encoding

--- ch02_basic_networking/tcp_client.py
+++ ch02_basic_networking/tcp_client.py
@@ -1,25 +1,3 @@
-# import socket
-
-# target_host = "www.google.com"
-# target_port = 80
-
-# # Buat objek socket (IPv4, TCP)
-# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# # Hubungkan ke target
-# client.connect((target_host, target_port))
-
-# # Kirim permintaan HTTP GET
-# client.send(b"GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n")
-
-# # Terima respons (buffer 4096 bytes)
-# response = client.recv(4096)
-
-# print(response.decode())
-
-# # Tutup koneksi
-# client.close()
-
 import socket
 
 target_host = "www.google.com"
@@ -32,7 +10,6 @@ client.connect((target_host, target_port))
 request = f"GET / HTTP/1.1\r\nHost: {target_host}\r\n\r\n"
 
 client.send(request.encode())
-
 
 response = client.recv(4096)
 print(response.decode())
