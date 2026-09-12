99d03e9c5cdd19ab725af2e07a924ba1b6116a8a
rosan-f <fikrirausyan131@gmail.com>
2026-09-12 20:58:17 +0700

    feat: add tcp proxy

---
+++ ch02_basic_networking/tcp_proxy.py
@@ -0,0 +1,28 @@
+import sys
+import socket
+import threading
+
+
+HEX_FILTER = ''.join(
+    [chr(i) if len(repr(chr(i))) == 3 else '.' for i in range(256)]
+)
+
+
+def hexdump(src, length=16, show=True):
+    if isinstance(src, bytes):
+        src = src.decode()
+
+    result = list()
+    for i in range(0, len(src), length):
+        word = str(src[i:i+length])
+
+        printable = word.translate(HEX_FILTER)
+        hexa = ' '.join([f'{ord(c):02X}' for c in word])
+        hexwidht = length*3
+        result.append(f'{i:04X} {hexa:<{hexwidht}} {printable}')
+
+    if show:
+        for line in result:
+            print(line)
+    else:
+        return result
