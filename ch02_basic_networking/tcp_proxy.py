5080510ff06922dc4cad0998bd4226a063273481
rosan-f <fikrirausyan131@gmail.com>
2026-09-14 15:37:53 +0700

    feat:  add proxy_handler function

--- ch02_basic_networking/tcp_proxy.py
+++ ch02_basic_networking/tcp_proxy.py
@@ -40,3 +40,48 @@ def receive_from(connection):
     except Exception as e:
         pass
     return buffer
+
+def proxy_habdler(client_socket, remote_host, remote_port, receive_first):
+    remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    remote_socket.connect((remote_host, remote_port))
+
+    if receive_first:
+        remote_buffer = receive_form(remote_socket)
+        hexdump(remote_buffer)
+
+    while True:
+        local_buffer = receive_from(client_socket)
+        if len(local_buffer):
+            line = "[==>] Received %d bytes from localhost." % len(local_buffer)
+        print(line)
+        hexdump(local_buffer)
+
+        local_buffer = request_handler(local_buffer)
+        remote_socket.send(local_buffer)
+        print("[==>] sent to remote")
+
+        remote_buffer = receive_from(remote_socket)
+        if len(remote_buffer):
+            print("[==>] Received %d bytes to localhost. " % len(local_buffer))
+            print(line)
+            hexdump(local_buffer)
+
+            local_buffer = request_handler(local_buffer)
+            remote_socket.send(local_buffer)
+            print("[==>] sent to remote.")
+
+            remote_buffer = receive_from(remote_socket)
+            if len(remote_buffer):
+                print("[<==] Received %d bytes from remote." % len(remote_buffer))
+                hexdump(local_buffer)
+
+                remote_buffer = response_handler(remote_buffer)
+                client_socket.send(remote_buffer)
+                print("[<==] sent to localhost")
+
+            if not len(local_buffer) or not len(remote_buffer):
+                client_socket.close()
+                remote_socket.close()
+                print("[*] No more data. Closing connections")
+                break
+
